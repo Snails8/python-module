da6dcf10bf50347c5c50e9e753331aab8fbc7944
Kaito Suzuki <snails8d@gmail.com>
2023-06-06 11:14:22 +0900

    add: merge files

--- merge_csv_files/src/main.py
+++ merge_csv_files/src/main.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
 DOWNLOAD_FOLDER = "../../../../Downloads/"
-FILE_A =  DOWNLOAD_FOLDER + 'testa.csv'
-FILE_B = DOWNLOAD_FOLDER + 'test.csv'
+FILE_A =  DOWNLOAD_FOLDER + 'test.xlsx'
+FILE_B = DOWNLOAD_FOLDER + 'sample.csv'
 
-OUTPUT_PATH = 'data/result.csv'
+OUTPUT_PATH = DOWNLOAD_FOLDER + 'result.csv'
+OUTPUT_EXCEL_PATH = DOWNLOAD_FOLDER + 'result.xlsx'
 
 def compare_csv_files(file_a: str, file_b: str):
   """csv ファイルを比較し、AにはなくてBにある行をcsvとして出力する
@@ -29,9 +30,14 @@ def compare_csv_files(file_a: str, file_b: str):
 
 
 def merge_df_add_columns(file_a: str, file_b: str):
-  df_a = pd.read_csv(file_a)
+  df_a = pd.read_excel(file_a, sheet_name="追加シート")
   df_b = pd.read_csv(file_b)
   
-  merged_df = pd.merge(df_a, df_b[['code', 'column1', 'column2']], on='code', how='left')
+  merged_df = pd.merge(df_a, df_b[['name', 'code', 'is_red', 'is_green']], on='name', how='left')
+  
+  merged_df.to_csv(OUTPUT_PATH)
+  merged_df.to_excel(OUTPUT_EXCEL_PATH)
+  print('------------- merge done -----------------')
+
 
 merge_df_add_columns(FILE_A, FILE_B)
