c5dcd8736cafaa6eac6957bed94a10f89111b31e
Kaito Suzuki <snails8d@gmail.com>
2023-06-16 20:56:41 +0900

    add: main.py sample

---
+++ pandas-playground/src/main.sample.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from domain.count_column import main
+
+DOWNLOAD_FOLDER = "../../../../../../Downloads/"
+FILE_A =  DOWNLOAD_FOLDER + 'test.csv'
+FILE_B = DOWNLOAD_FOLDER + 'test.csv'
+OUTPUT_PATH = 'data/result.csv'
+
+# df = pd.read_csv(FILE_A, dtype={'code': str})
+# print(df['code'])
+
+def main():
+  # encoding = 'cp932'
+  # df_a = pd.read_csv(FILE_A)
+  df_b = pd.read_csv(FILE_B) 
+  
+  print('--------------- done ------------------')
+
+main()
